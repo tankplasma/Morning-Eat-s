2d095ff8ef832136da7c017fdaeeb0d9a2e85737
tankplasma <adrien.alard@gmail.com>
2020-09-28 09:28:15 +0200

    set-up

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("inc/header.inc.php"); ?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h3>Bienvenue sur mon site, vous pouvez vous balader et regarder chaque recoin de ce site (des petits easter eggs s'y cachent 😉) </h3>
+<br>
+<br>
+<h5>Vous pouvez aussi vous créez un compte pour pouvoir accéder à la page Admin afin de modifier les informations des expériences </h5>
+<h5>ou bien vous connecter à mon compte : Guillaume / PHPynov18</h5>
+<br>
+<br>
+<h5>Et pour information mes données Expériences et Education proviennent d'une base de données SQL</h5>
+
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,42 @@
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Employé Polyvalent</h3>
+            <div class="subheading mb-3">LIDL</div>
+            <p>Caissier, boulanger, homme de ménage, rangement rayon, mise en rayon,...</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">Février 2020 - maintenant</span>
+          </div>
+        </div>
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Jeune Devellopeur</h3>
+            <div class="subheading mb-3">Spécialité ISN Lycée</div>
+            <p>J'ai commencé à réellement le code en Terminale S et cela ma passionné directement</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">Septembre 2018 - Juillet 2019</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Soutien Scolaire</h3>
+            <div class="subheading mb-3"></div>
+            <p>Cours particulier à des collégiens sur les matières scientifiques</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2016 - maintenant</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+          <div class="resume-content">
+            <h3 class="mb-0">Baby-sitting</h3>
+            <div class="subheading mb-3"></div>
+            <p>Garde, aller les chercher à l'école, leur faire l'étude....</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">2014 - 2019</span>
+          </div>
+        </div>
