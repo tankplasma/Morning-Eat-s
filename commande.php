43e437c068d63cc1455b7e2839e25916b8836a81
Marie <47717194+Clawchette@users.noreply.github.com>
2020-09-28 11:08:16 +0200

    ajout page commande

---
+++ commande.php
@@ -0,0 +1,32 @@
+<?php include("inc/header.inc.php"); ?>
+<div style="padding-left:10px;">
+    <h1>Menu choisi</h1>
+
+    <br><div class="row">
+        <div class="col">
+            <img style="height: 300px; width: 450px;" src="img/burger.jpg" alt="Image du menu choisi">
+        </div>
+        <div class="col">
+            <h3>Menu Burger</h3>
+            <p>de [Nom du restaurant]</p><br>
+        </div>
+    </div>
+
+
+    <br><div>
+        <h5>Ce menu contient :</h5>
+        <p>- Un burger <br>- Une frite <br>- Une boisson</p>
+    </div>
+
+    <br><div>
+        <h5>Allergènes :</h5>
+        <p style="font-style: italic">Ce menu ne contient pas d'allergènes.</p>
+    </div>
+
+    <button type="button" style="margin-bottom:20px;" class="btn btn-dark">Commander</button>
+
+</div>
+
+</body>
+
+</html>
